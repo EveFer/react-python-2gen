b0e7b2df9127d5f93314791b3eb1d7c1c2da23b7
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2021-08-02 23:40:12 -0500

    Project Base

---
+++ pages/blog/[slug].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Post() {
+    return (
+        <div>
+            <h1>En algun post</h1>
+        </div>
+    )
+}
---
+++ pages/nosotros.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function nosotros() {
+    return (
+        <div>
+            <h1>Hola Estamos en nosotros</h1>
+        </div>
+    )
+}
